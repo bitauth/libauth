f599364c88485874401c5d782818aa3bcace0eb8
Jason Dreyzehner <jason@dreyzehner.com>
2019-08-20 14:28:14 -0400

    test(stringify): improve docs, add tests

---
+++ src/lib/utils/log.spec.ts
@@ -0,0 +1,24 @@
+// tslint:disable:no-expression-statement no-magic-numbers
+import test from 'ava';
+
+import { stringify } from './log';
+
+test('stringify', t => {
+  t.deepEqual(stringify(BigInt(0)), '"<bigint: 0n>"');
+  t.deepEqual(stringify({ a: BigInt(0) }), '{\n  "a": "<bigint: 0n>"\n}');
+  t.deepEqual(stringify(Uint8Array.of(32, 32)), '"<Uint8Array: 0x2020>"');
+  t.deepEqual(
+    stringify({ b: Uint8Array.of(32, 32) }),
+    '{\n  "b": "<Uint8Array: 0x2020>"\n}'
+  );
+  t.deepEqual(stringify((x: number) => x * 2), '"<function: x => x * 2>"');
+  t.deepEqual(
+    stringify({ c: (x: number) => x * 2 }),
+    '{\n  "c": "<function: x => x * 2>"\n}'
+  );
+  t.deepEqual(stringify(Symbol('A')), '"<symbol: Symbol(A)>"');
+  t.deepEqual(
+    stringify({ d: Symbol('A') }),
+    '{\n  "d": "<symbol: Symbol(A)>"\n}'
+  );
+});
--- src/lib/utils/log.ts
+++ src/lib/utils/log.ts
@@ -1,26 +1,48 @@
 import { binToHex } from './hex';
 
-export const stringify = (object: object, spacing = 2) =>
+/**
+ * A safe method to `JSON.stringify` a value, useful for debugging and logging
+ * purposes.
+ *
+ * @remarks
+ * Without modifications, `JSON.stringify` has several shortcomings in
+ * debugging and logging usage:
+ * - throws when serializing anything containing a `bigint`
+ * - `Uint8Array`s are often serialized in base 10 with newlines between each
+ *   index item
+ * - `functions` and `symbols` are not clearly marked
+ *
+ * This method is more helpful in these cases:
+ * - `bigint`: `0n` => `<bigint: 0n>`
+ * - `Uint8Array`: `Uint8Array.of(0,0)` => `<Uint8Array: 0x0000>`
+ * - `function`: `(x) => x * 2` => `<function: (x) => x * 2>`
+ * - `symbol`: `Symbol(A)` => `<symbol: Symbol(A)>`
+ *
+ * @param value the data to serialize
+ * @param spacing the number of spaces to use in
+ */
+// tslint:disable-next-line: no-any
+export const stringify = (value: any, spacing = 2) =>
   JSON.stringify(
-    object,
+    value,
     // tslint:disable-next-line:cyclomatic-complexity
-    (_, value) => {
-      const type = typeof value;
+    (_, item) => {
+      const type = typeof item;
       const name =
         // tslint:disable-next-line: no-unsafe-any
-        type === 'object' ? value.constructor && value.constructor.name : type;
+        type === 'object' ? item.constructor && item.constructor.name : type;
       switch (name) {
         case 'Uint8Array':
-          return `<Uint8Array: 0x${binToHex(value as Uint8Array)}>`;
+          return `<Uint8Array: 0x${binToHex(item as Uint8Array)}>`;
         case 'bigint':
-          return `<bigint: ${(value as bigint).toString()}n>`;
+          return `<bigint: ${(item as bigint).toString()}n>`;
         case 'function':
         case 'symbol':
           // tslint:disable-next-line: ban-types
-          return `<${name}: ${(value as symbol | Function).toString()}>`;
+          return `<${name}: ${(item as symbol | Function).toString()}>`;
 
         default:
-          return value;
+          return item;
       }
     },
     spacing

0f735872fcdf8980565e5f77165ce554f4663309
Jason Dreyzehner <jason@dreyzehner.com>
2019-11-20 21:50:55 -0500

    test: add tests for utf8 utils

--- src/lib/utils/utf8.spec.ts
+++ src/lib/utils/utf8.spec.ts
@@ -1,5 +1,31 @@
 // tslint:disable:no-expression-statement
 import test from 'ava';
+import * as fc from 'fast-check';
 
-test.todo('utf8ToBin');
-test.todo('binToUtf8');
+import { hexToBin } from './hex';
+import { binToUtf8, utf8ToBin } from './utf8';
+
+const maxUint8Number = 255;
+const fcUint8Array = (minLength: number, maxLength: number) =>
+  fc
+    .array(fc.integer(0, maxUint8Number), minLength, maxLength)
+    .map(a => Uint8Array.from(a));
+
+test('utf8ToBin & ', t => {
+  t.deepEqual(utf8ToBin('👍'), hexToBin('f09f918d'));
+});
+
+test('binToUtf8', t => {
+  t.deepEqual(binToUtf8(hexToBin('f09f918d')), '👍');
+});
+
+const testBinLength = 100;
+test('utf8ToBin <-> binToUtf8', t => {
+  const inverse = fc.property(
+    fcUint8Array(0, testBinLength),
+    input => binToUtf8(utf8ToBin(binToUtf8(input))) === binToUtf8(input)
+  );
+  t.notThrows(() => {
+    fc.assert(inverse);
+  });
+});
--- src/lib/utils/utf8.ts
+++ src/lib/utils/utf8.ts
@@ -1,9 +1,27 @@
-// TODO: clean up - this implementation comes from:
-// https://github.com/google/closure-library/blob/8598d87242af59aac233270742c8984e2b2bdbe0/closure/goog/crypt/crypt.js
-// consider switching to:
-// https://github.com/mathiasbynens/utf8.js/blob/master/utf8.js
+/**
+ * This implementations is derived from:
+ * https://github.com/google/closure-library/blob/8598d87242af59aac233270742c8984e2b2bdbe0/closure/goog/crypt/crypt.js
+ *
+ * Copyright 2008 The Closure Library Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 // tslint:disable:cyclomatic-complexity no-let no-if-statement no-magic-numbers no-object-mutation no-expression-statement no-bitwise
+/**
+ * Interpret a string as UTF-8 and encode it as a Uint8Array.
+ * @param utf8 the string to encode
+ */
 export const utf8ToBin = (utf8: string) => {
   const out = [];
   let p = 0;
@@ -32,46 +50,11 @@ export const utf8ToBin = (utf8: string) => {
   }
   return new Uint8Array(out);
 };
-// tslint:enable:cyclomatic-complexity no-let no-if-statement no-magic-numbers no-object-mutation no-expression-statement no-bitwise
 
-// also compare to:
-// https://gist.github.com/joni/3760795
-// https://gist.github.com/pascaldekloe/62546103a1576803dade9269ccf76330
-// and:
-
-// function stringFromUTF8Array(data)
-// {
-//   const extraByteMap = [ 1, 1, 1, 1, 2, 2, 3, 0 ];
-//   var count = data.length;
-//   var str = "";
-
-//   for (var index = 0;index < count;)
-//   {
-//     var ch = data[index++];
-//     if (ch & 0x80)
-//     {
-//       var extra = extraByteMap[(ch >> 3) & 0x07];
-//       if (!(ch & 0x40) || !extra || ((index + extra) > count))
-//         return null;
-
-//       ch = ch & (0x3F >> extra);
-//       for (;extra > 0;extra -= 1)
-//       {
-//         var chx = data[index++];
-//         if ((chx & 0xC0) != 0x80)
-//           return null;
-
-//         ch = (ch << 6) | (chx & 0x3F);
-//       }
-//     }
-
-//     str += String.fromCharCode(ch);
-//   }
-
-//   return str;
-// }
-
-// tslint:disable:cyclomatic-complexity no-let no-if-statement no-magic-numbers no-object-mutation no-expression-statement no-bitwise
+/**
+ * Decode a Uint8Array as a UTF-8 string.
+ * @param bytes the Uint8Array to decode
+ */
 export const binToUtf8 = (bytes: Uint8Array) => {
   const out = [];
   let pos = 0;
@@ -102,4 +85,3 @@ export const binToUtf8 = (bytes: Uint8Array) => {
   }
   return out.join('');
 };
-// tslint:enable:cyclomatic-complexity no-let no-if-statement no-magic-numbers no-object-mutation no-expression-statement no-bitwise
